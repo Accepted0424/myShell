f4b17f5102938bbfcfa90446c0a6e7fd63c9b9fd
lhy <23373112@buaa.edu.cn>
2025-04-28 20:18:22 +0800

    sync

--- myShell.c
+++ myShell.c
@@ -13,90 +13,60 @@
 // color define
 #define BLUE "\e[1;34m"
 #define YELLOW "\e[1;33m"
-#define WHITE "\e[1;37m"
+#define WHITE "\e[0;37m"
 #define RED "\e[1;31m"
 #define GREEN "\e[1;32m"
 
-void execute_command(char *args[], int input_fd, int output_fd) {
-    pid_t pid = fork();
-    if (pid == 0) {
-        // Child process
+#define EXECUTE_BUILTIN_COMMAND(func, args) \
+    do {                                    \
+        func(args);                         \
+    } while (0);
 
-        // 如果存在输入重定向，则将输入重定向到指定文件描述符
-        if (input_fd != 0) {
-            dup2(input_fd, 0);
-            close(input_fd);
+void shell_interact();
+void shell_script();
+void execute_command(char *args[], int input_fd, int output_fd);
+void execute_out_command(char *args[], int input_fd, int output_fd);
+void execute_builtin_command(char *args[], int input_fd, int output_fd);
+void display_hello();
+void display_prefix();
+
+const char *builtins[] = {
+    "cd",
+    "exit",
+    NULL 
+};
+
+int main(int argc, char *argv[]) {
+    display_hello();
+    if (argc == 1) {
+        // 交互模式
+        shell_interact();
+    } else if (argc == 2) {
+        // 脚本模式
+        FILE *file = fopen(argv[1], "r");
+        if (file == NULL) {
+            perror("fopen");
+            exit(1);
         }
-
-        // 如果存在输出重定向，则将输出重定向到指定文件描述符
-        if (output_fd != 1) {
-            dup2(output_fd, 1);
-            close(output_fd);
-        }
-
-        // 执行外部命令
-        execvp(args[0], args);
-        perror("execvp");
-        exit(1);
-    } else if (pid > 0) {
-        // Parent process
-        wait(NULL);
+        // 重定向标准输入到脚本文件
+        dup2(fileno(file), STDIN_FILENO);
+        fclose(file);
+        shell_script();
     } else {
-        // Fork failed
-        perror("fork");
+        fprintf(stderr, "Usage: %s [script_file]\n", argv[0]);
         exit(1);
     }
+
+    return 0;
 }
 
-void display_hello() {
-    printf("Welcome to lhy's Shell!\n");
-    printf("This is the final project of UNIX at BUAA.\n");
-    printf("Author: Haoyu Luo\n");
-    printf("Student ID: 23373112\n");
-    printf(" _      _   _ __   __        ____   _   _  _____  _      _ \n");   
-    printf("| |    | | | |\\ \\ / /       / ___| | | | || ____|| |    | |\n");
-    printf("| |    | |_| | \\ V /  _____ \\___ \\ | |_| ||  _|  | |    | |    \n");
-    printf("| |___ |  _  |  | |  |_____| ___) ||  _  || |___ | |___ | |___ \n");
-    printf("|_____||_| |_|  |_|         |____/ |_| |_||_____||_____||_____|\n");
-}
-
-void display_prefix() {
-    char *home = getenv("HOME");
-    char *current_dir = getcwd(NULL, 0);
-    if (current_dir == NULL) {
-        perror("getcwd");
-        exit(1);
-    }
-    struct passwd *pwd = getpwuid(getuid());
-    char *hostname = (char *)malloc(MAX_LINE);
-    if (gethostname(hostname, MAX_LINE) == -1) {
-        perror("gethostname");
-        exit(1);
-    }
-    hostname[strcspn(hostname, ".")] = 0;
-    hostname[strcspn(hostname, "\n")] = 0;
-    char *dir = strstr(current_dir, home);
-    char *prefix_dir = (char *)malloc(strlen(home) + 1);
-    if (dir != NULL) {
-        prefix_dir[0] = '~';
-        strcpy(prefix_dir + 1, dir + strlen(home));
-    } else {
-        prefix_dir = current_dir;
-    }
-    printf(RED "[lhy's Shell] " BLUE "%s@%s:" YELLOW "%s" WHITE "$ ", pwd->pw_name, hostname, prefix_dir);
-    free(current_dir);
-}
-
-int main() {
+void shell_interact() {
     char line[MAX_LINE];    // 存储输入的命令行
     char *args[MAX_ARGS];   // 命令行参数列表
 
-    display_hello();
-
     while (1) {
         display_prefix();
-        
-fflush(stdout);
+        fflush(stdout);
         // 读取输入命令行
         if (!fgets(line, MAX_LINE, stdin)) {
             break;
@@ -150,6 +120,121 @@ fflush(stdout);
             execute_command(args, 0, 1);
         }
     }
+}
 
+int isBuiltin(char *func_name) {
+    for (int i = 0; builtins[i] != NULL; i++) {
+        if (strcmp(func_name, builtins[i]) == 0) {
+            return 1;
+        }
+    }
     return 0;
+}
+
+void execute_command(char *args[], int input_fd, int output_fd) {
+    // 检查是否是内置命令
+    if (isBuiltin(args[0])) {
+        execute_builtin_command(args, input_fd, output_fd);
+        return;
+    } else {
+        execute_out_command(args, input_fd, output_fd);
+    }
+}
+
+void execute_out_command(char *args[], int input_fd, int output_fd) {
+    pid_t pid = fork();
+    if (pid == 0) {
+        // Child process
+
+        // 如果存在输入重定向，则将输入重定向到指定文件描述符
+        if (input_fd != 0) {
+            dup2(input_fd, 0);
+            close(input_fd);
+        }
+
+        // 如果存在输出重定向，则将输出重定向到指定文件描述符
+        if (output_fd != 1) {
+            dup2(output_fd, 1);
+            close(output_fd);
+        }
+
+        // 执行外部命令
+        execvp(args[0], args);
+        perror("execvp");
+        exit(1);
+    } else if (pid > 0) {
+        // Parent process
+        wait(NULL);
+    } else {
+        // Fork failed
+        perror("fork");
+        exit(1);
+    }
+}
+
+void execute_builtin_command(char *args[], int input_fd, int output_fd) {
+    EXECUTE_BUILTIN_COMMAND(args[0], args);
+}
+
+void display_hello() {
+    printf("Welcome to lhy's Shell!\n");
+    printf("This is the final project of UNIX at BUAA.\n");
+    printf("Author: Haoyu Luo\n");
+    printf("Student ID: 23373112\n");
+    printf(" _      _   _ __   __        ____   _   _  _____  _      _ \n");   
+    printf("| |    | | | |\\ \\ / /       / ___| | | | || ____|| |    | |\n");
+    printf("| |    | |_| | \\ V /  _____ \\___ \\ | |_| ||  _|  | |    | |    \n");
+    printf("| |___ |  _  |  | |  |_____| ___) ||  _  || |___ | |___ | |___ \n");
+    printf("|_____||_| |_|  |_|         |____/ |_| |_||_____||_____||_____|\n\n");
+}
+
+void display_prefix() {
+    char *home = getenv("HOME");
+    char *current_dir = getcwd(NULL, 0);
+    if (current_dir == NULL) {
+        perror("getcwd");
+        exit(1);
+    }
+    struct passwd *pwd = getpwuid(getuid());
+    char *hostname = (char *)malloc(MAX_LINE);
+    if (gethostname(hostname, MAX_LINE) == -1) {
+        perror("gethostname");
+        exit(1);
+    }
+    hostname[strcspn(hostname, ".")] = 0;
+    hostname[strcspn(hostname, "\n")] = 0;
+    char *dir = strstr(current_dir, home);
+    char *prefix_dir = (char *)malloc(strlen(home) + 1);
+    if (dir != NULL) {
+        prefix_dir[0] = '~';
+        strcpy(prefix_dir + 1, dir + strlen(home));
+    } else {
+        prefix_dir = current_dir;
+    }
+    printf(RED "[lhy's Shell] " BLUE "%s@%s:" YELLOW "%s" WHITE "$ ", pwd->pw_name, hostname, prefix_dir);
+    free(current_dir);
+}
+
+// builtin command
+void cd(char *args[]) {
+    if (args[1] == NULL) {
+        fprintf(stderr, "cd: missing argument\n");
+        return;
+    }
+    if (chdir(args[1]) != 0) {
+        perror("cd");
+    }
+}
+
+void exit_lsh(char *args[]) {
+    exit(0);
+}
+
+void echo(char *args[]) {
+    int i = 1;
+    while (args[i] != NULL) {
+        printf("%s ", args[i]);
+        i++;
+    }
+    printf("\n");
 }
