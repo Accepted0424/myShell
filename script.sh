12078d215e816752a2f257807ef20bbc76fd8db1
lhy <23373112@buaa.edu.cn>
2025-05-05 21:59:39 +0800

    update test script

--- script.sh
+++ script.sh
@@ -1,2 +1,6 @@
-cat < myShell.c | grep include > test_13
-echo end
+echo ------ script test begin -------
+echo test command: find "include" in myShell.c
+echo display result:
+cat < myShell.c | grep include > test
+cat test
+echo ------ script test end -------
